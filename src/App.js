333a72e949285e7c76eb3cad91f41d3656fb0faf
bsy3764 <bsy3764@gmail.com>
2021-11-03 22:22:37 +0900

    todo list style

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import TodoInsert from './todoComponents/TodoInsert';
 import TodoTemplate from './todoComponents/TodoTemplate';
+import TodoList from './todoComponents/TodoList';
 
 const App = () => {
-  return <TodoTemplate>Todo App</TodoTemplate>;
-};
+  return (
+    <TodoTemplate>
+      <TodoInsert/>
+      <TodoList/>
+    </TodoTemplate>
+  )};
 
 export default App;
