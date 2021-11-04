e8513cd17fbc382a1b702fa4c317ba8428640d46
bsy3764 <bsy3764@gmail.com>
2021-11-04 22:26:11 +0900

    todo add, update, remove

--- src/App.js
+++ src/App.js
@@ -2,12 +2,62 @@ import './App.css';
 import TodoInsert from './todoComponents/TodoInsert';
 import TodoTemplate from './todoComponents/TodoTemplate';
 import TodoList from './todoComponents/TodoList';
+import { useCallback, useRef, useState } from 'react';
 
 const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: 'test1',
+      checked: true
+    },
+    {
+      id: 2,
+      text: 'test2',
+      checked: true
+    },
+    {
+      id: 3,
+      text: 'test3',
+      checked: false
+    }
+  ]);
+
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    text => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    }, [todos]
+  );
+
+  const onRemove = useCallback(
+    id => {
+      setTodos(todos.filter(todo => todo.id !== id));
+    }, [todos]
+  );
+
+  const onToggle = useCallback(
+    id => {
+      setTodos(
+        todos.map(todo =>
+          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+        ),
+      );
+    },
+    [todos],
+  );
+
   return (
     <TodoTemplate>
-      <TodoInsert/>
-      <TodoList/>
+      <TodoInsert onInsert={onInsert}/>
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle}/>
     </TodoTemplate>
   )};
 
